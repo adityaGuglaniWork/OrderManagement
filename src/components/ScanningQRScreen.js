f99a905f4ae29a9897ad49fd31dcbee9bf2d4c35
Aditya <aditya.guglani@vinsol.com>
2023-01-25 22:19:11 +0530

    Show alert for invalid codes

--- src/components/ScanningQRScreen.js
+++ src/components/ScanningQRScreen.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { TouchableOpacity, Text, Linking, View, Image, ImageBackground, BackHandler } from 'react-native';
+import { Alert, Text, View } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { Dimensions } from 'react-native';
 const deviceWidth = Dimensions.get('screen').width;
@@ -7,9 +7,22 @@ const deviceHeight = Dimensions.get('screen').height;
 
 class Scan extends Component {
     onSuccess = (e) => {
-        console.log(e);
-        const check = e.data.substring(0, 4);
-        console.log('scanned data' + check);
+        const showNotSupportedDialog = () => {
+            Alert.alert("Scanned Code", "Code not supported in the app");
+        }
+
+        try {
+            const data = JSON.parse(e.data);
+            const appData = JSON.parse(data);
+            console.log(`TYPE: ${appData.type} ID: ${appData.id}`);
+            if (appData.type && appData.id) {
+
+            } else {
+                showNotSupportedDialog();
+            }
+        } catch (e) {
+            showNotSupportedDialog();
+        }
     }
     render() {
         return (
@@ -36,111 +49,13 @@ const styles = {
     scrollViewStyle: {
         flex: 1,
         justifyContent: 'flex-start',
-        backgroundColor: '#2196f3'
-    },
-    header: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: '10%',
-        paddingLeft: 15,
-        paddingTop: 10,
-        width: deviceWidth,
-    },
-    textTitle: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        textAlign: 'center',
-        padding: 16,
-        color: 'white'
-    },
-    textTitle1: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        textAlign: 'center',
-        padding: 16,
-        color: 'white'
-    },
-    cardView: {
-        width: deviceWidth - 32,
-        height: deviceHeight - 350,
-        alignSelf: 'center',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        borderRadius: 10,
-        padding: 25,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: '10%',
-        backgroundColor: 'white'
-    },
-    scanCardView: {
-        width: deviceWidth - 32,
-        height: deviceHeight / 2,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        padding: 25,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10,
-        backgroundColor: 'white'
-    },
-    buttonWrapper: {
-        display: 'flex', 
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    buttonScan: {
-        borderWidth: 2,
-        borderRadius: 10,
-        borderColor: '#258ce3',
-        paddingTop: 5,
-        paddingRight: 25,
-        paddingBottom: 5,
-        paddingLeft: 25,
-        marginTop: 20
-    },
-    buttonScan2: {
-        marginLeft: deviceWidth / 2 - 50,
-        width: 100,
-        height: 100,
-    },
-    descText: {
-        padding: 16,
-        textAlign: 'center',
-        fontSize: 16
-    },
-    highlight: {
-        fontWeight: '700',
+        backgroundColor: 'maroon'
     },
     centerText: {
         flex: 1,
         textAlign: 'center',
-        fontSize: 18,
-        padding: 32,
+        fontSize: 10,
+        margin: 32,
         color: 'white',
-    },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    bottomContent: {
-       width: deviceWidth,
-       height: 120,
-    },
-    buttonTouchable: {
-        fontSize: 21,
-        backgroundColor: 'white',
-        marginTop: 32,
-        width: deviceWidth - 62,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 44
-    },
-    buttonTextStyle: {
-        color: 'black',
-        fontWeight: 'bold',
     }
 }
